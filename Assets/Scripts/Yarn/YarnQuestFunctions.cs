b7491b095f44ae25d9e836bd9dd9e2ef2478a8f2
Luke Doty <lukedoty@gatech.edu>
2026-02-24 19:36:50 -0500

    Dialogue cleanup and testing

--- Assets/Scripts/Yarn/YarnQuestFunctions.cs
+++ Assets/Scripts/Yarn/YarnQuestFunctions.cs
@@ -8,9 +8,9 @@ public static class YarnQuestFunctions
     [YarnFunction("is-quest-complete")]
     public static bool IsQuestComplete(string questID) => GameManager.Quest.IsQuestComplete(questID);
 
-    [YarnFunction("assign-quest")]
-    public static bool AssignQuest(string questID) => GameManager.Quest.AssignQuest(questID);
+    [YarnCommand("assign-quest")]
+    public static void AssignQuest(string questID) => GameManager.Quest.AssignQuest(questID);
 
-    [YarnFunction("complete-quest")]
-    public static bool CompleteQuest(string questID) => GameManager.Quest.CompleteQuest(questID);
+    [YarnCommand("complete-quest")]
+    public static void CompleteQuest(string questID) => GameManager.Quest.CompleteQuest(questID);
 }
