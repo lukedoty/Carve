3e59f470fef3d0477d9dbabfa38d73ff2f55774f
Sophia Regnault <sophiaregnault@icloud.com>
2026-03-02 13:54:42 -0500

    Canvas Appears

--- Assets/Scripts/Environment/GenerateSign.cs
+++ Assets/Scripts/Environment/GenerateSign.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Numerics;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class GenerateSign : MonoBehaviour
 {
     public GameObject signPanel;
+    public GameObject signGraphic;
     public float signDisplacement;
     public float signTop;
     [Serializable]
@@ -18,6 +21,8 @@ public class GenerateSign : MonoBehaviour
         [Range (0,359)]
         public int direction;
     }
+    public int test;
+    public Dictionary<SignType, Color> signTypeColors;
 
     public List<SignInfo> signs;
 
@@ -40,6 +45,16 @@ public class GenerateSign : MonoBehaviour
             GameObject panel = Instantiate(signPanel, this.transform);
             panel.transform.localScale = panel.transform.localScale / this.transform.localScale.x;
             panel.transform.position = panel.transform.position + new UnityEngine.Vector3(0, signTop - (signDisplacement * signNum), 0.1f);
+
+            GameObject graphic = Instantiate(signGraphic, this.transform);
+            graphic.transform.localScale = graphic.transform.localScale / this.transform.localScale.x;
+            graphic.transform.position = graphic.transform.position + new UnityEngine.Vector3(0, signTop - (signDisplacement * signNum), 0.15f);
+
+            TextMeshProUGUI signText = graphic.GetComponentInChildren<TextMeshProUGUI>();
+            signText.SetText(sign.text);
+
+
+
             signNum++;
         }
     }
