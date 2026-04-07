9855b7e102806dcd8f33586a9fcec2714e1f9a98
Sophia Regnault <90007008+EchoEcho33@users.noreply.github.com>
2026-04-07 11:13:48 -0400

    Editor Dropdown

--- Assets/Scripts/Environment/OverworldSticker.cs
+++ Assets/Scripts/Environment/OverworldSticker.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEditor;
+using Mono.Cecil;
+using System;
+using System.Data.Common;
 
 [RequireComponent(typeof(Collider))]
 public class OverworldSticker : MonoBehaviour
 {
-    [SerializeField]
     private string m_stickerID;
 
     public void Start()
@@ -13,6 +17,21 @@ public class OverworldSticker : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        SpriteRenderer stickerRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        if (stickerRenderer == null)
+        {
+            Debug.LogError($"Overworld sticker of ID \"{m_stickerID}\" has no child sprite.");
+            return;
+        }
+
+        Sprite stickerSprite = GameManager.Sticker.Registry[m_stickerID].StickerImage;
+        stickerRenderer.sprite = stickerSprite;
+    }
+
+    public void setID(string ID)
+    {
+        m_stickerID = ID;
     }
 
     void OnTriggerEnter(Collider other)
@@ -21,6 +40,43 @@ public class OverworldSticker : MonoBehaviour
         {
             GameManager.Sticker.ObtainSticker(m_stickerID);
             Destroy(gameObject);
+        }
+    }
+}
+
+[CustomEditor(typeof(OverworldSticker))]
+public class OverworldStickerEditor : Editor
+{
+
+    
+    string[] IDs = {};
+    int i = 0;
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        StickerManager stickerManager = FindFirstObjectByType<StickerManager>();
+        List<Sticker> stickerList = stickerManager.RegistryList;
+
+        int j = 0;
+        foreach (Sticker sticker in stickerList)
+        {
+            if (sticker != null)
+            {
+                IDs[j] = sticker.ID;
+                j++;
+            }
+        }
+        OverworldSticker overworldSticker = target as OverworldSticker;
+        if (IDs.Length > 0)
+        {
+            i = EditorGUILayout.Popup(i, IDs);
+            overworldSticker.setID(IDs[i]);
+            EditorUtility.SetDirty(target);
+        } 
+        else
+        {
+            overworldSticker.setID("LittleSteppe");
+            EditorGUILayout.LabelField("ERROR: Sticker List not found or no stickers contained in the registry.");
         }
     }
 }
