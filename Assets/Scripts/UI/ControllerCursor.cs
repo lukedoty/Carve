60566be86b66636d8a4b7a3392380beaff94ce86
Joy S Zwiernikowski <joyzski1@gmail.com>
2026-04-13 11:09:30 -0400

    Added slope hover system for journals

--- Assets/Scripts/UI/ControllerCursor.cs
+++ Assets/Scripts/UI/ControllerCursor.cs
@@ -21,21 +21,7 @@ public class ControllerCursor : MonoBehaviour
         Vector2 currDistance = m_navigate.ReadValue<Vector2>();
         if (currDistance.x != 0 || currDistance.y != 0)
         {
-            transform.Translate(m_navigate.ReadValue<Vector2>() * Time.deltaTime * 200);
-            if (transform.position.x > m_mapPos.x + m_mapWidth/2)
-            {
-                transform.position = new Vector2(m_mapPos.x + m_mapWidth/2, transform.position.y);
-            } else if (transform.position.x < m_mapPos.x - m_mapWidth/2)
-            {
-                transform.position = new Vector2(m_mapPos.x - m_mapWidth/2, transform.position.y);
-            }
-            if (transform.position.y > m_mapPos.y + m_mapHeight/2)
-            {
-                transform.position = new Vector2(transform.position.x, m_mapPos.y + m_mapHeight/2);
-            } else if (transform.position.y < m_mapPos.y - m_mapHeight/2)
-            {
-                transform.position = new Vector2(transform.position.x, m_mapPos.y - m_mapHeight/2);
-            }
+            transform.Translate(m_navigate.ReadValue<Vector2>() * Time.deltaTime * 300);
         }
     }
 }
--- Assets/Scripts/UI/UISlope.cs
+++ Assets/Scripts/UI/UISlope.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class UISlope : MonoBehaviour
 {
     [SerializeField]
     private string m_name;
-    public string Name => m_name;
 
     [SerializeField]
     private Sprite m_slopeIcon;
-    public Sprite icon => m_slopeIcon;
 
 
     [SerializeField]
     private Sprite m_slopeImage;
-    public Sprite image => m_slopeImage;
+
+    [SerializeField]
+    private GameObject m_journalName;
+    [SerializeField]
+    private Image m_journalIcon;
+    [SerializeField]
+    private Image m_journalImage;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        m_journalName.GetComponent<TextMeshProUGUI>().text = m_name;
+        m_journalIcon.GetComponent<Image>().sprite = m_slopeIcon;
+        m_journalImage.GetComponent<Image>().sprite = m_slopeImage;
+
+    }
 }
