97b57cddb07409e1951dbce0b9dd5d48797ab1ba
Vivian Gu <vgu6@gatech.edu>
2026-04-22 14:45:00 -0400

    dialogue box disappears when out of range

--- Assets/Scripts/Yarn/DialogueController.cs
+++ Assets/Scripts/Yarn/DialogueController.cs
@@ -7,14 +7,30 @@ public class DialogueController : MonoBehaviour
     [SerializeField]
     private GameObject m_player;
 
+    [SerializeField]
+    private float m_maxDialogueDistance = 5f;
+
     private SkiController m_playerSkiController;
 
     private DialogueRunner m_dialogueRunner;
+
+    private Transform m_dialogueSpeaker;
 
     private void Awake()
     {
         m_playerSkiController = m_player.GetComponent<SkiController>();
         m_dialogueRunner = GetComponent<DialogueRunner>();
+    }
+
+    private void Update()
+    {
+        if (m_dialogueSpeaker == null || !m_dialogueRunner.IsDialogueRunning) return;
+
+        float distance = Vector3.Distance(m_player.transform.position, m_dialogueSpeaker.position);
+        if (distance > m_maxDialogueDistance)
+        {
+            m_dialogueRunner.Stop().Forget();
+        }
     }
 
     public void OnDialogueStart()
@@ -29,10 +45,12 @@ public class DialogueController : MonoBehaviour
     {
         GameManager.Input.PlayerActions.Enable();
         //m_playerSkiController.Unfreeze();
+        m_dialogueSpeaker = null;
     }
 
-    public void StartDialogue(string nodeName)
+    public void StartDialogue(string nodeName, Transform speaker)
     {
+        m_dialogueSpeaker = speaker;
         m_dialogueRunner.StartDialogue(nodeName);
     }
 }
