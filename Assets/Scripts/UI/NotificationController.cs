61faae6a24b8153772d80202d7980c21f9c8f8ef
Vivian Gu <vgu6@gatech.edu>
2026-04-22 13:26:54 -0400

    add UI to notification system, wire in icons

--- Assets/Scripts/UI/NotificationController.cs
+++ Assets/Scripts/UI/NotificationController.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine.Events;
 
 public class NotificationController : MonoBehaviour
@@ -65,7 +64,7 @@ public class NotificationController : MonoBehaviour
             if (notification.Duration <= 0)
             {
                 m_notifications.RemoveAt(i);
-                Destroy(notification.Obj);
+                notification.Obj.GetComponent<NotificationView>().Hide();
             } else
             {
                 m_notifications[i] = notification;
@@ -76,26 +75,47 @@ public class NotificationController : MonoBehaviour
     private void InstantiateNotification(NotificationType type, string id)
     {
         GameObject notification = Instantiate(m_notificationPrefab, transform);
+        NotificationView view = notification.GetComponent<NotificationView>();
 
-        TMP_Text[] text = notification.GetComponentsInChildren<TMP_Text>();
-        
+        string header = "";
+        string body = "";
+        Sprite icon = null;
+
         switch (type)
         {
             case NotificationType.ObtainSticker:
-                text[0].text = m_obtainStickerHeader;
-                text[1].text = GameManager.Sticker.Registry[id].Name;
+                Sticker sticker = GameManager.Sticker.Registry[id];
+                header = m_obtainStickerHeader;
+                body = sticker.Name;
+                icon = sticker.StickerImage;
                 break;
             case NotificationType.AssignQuest:
-                text[0].text = m_assignQuestHeader;
-                text[1].text = GameManager.Quest.Registry[id].Name;
+                header = m_assignQuestHeader;
+                body = GameManager.Quest.Registry[id].Name;
                 break;
             case NotificationType.CompleteQuest:
-                text[0].text = m_completeQuestHeader;
-                text[1].text = GameManager.Quest.Registry[id].Name;
+                Quest quest = GameManager.Quest.Registry[id];
+                header = m_completeQuestHeader;
+                body = quest.Name;
+                icon = ResolveQuestRewardIcon(quest);
                 break;
         }
 
+        view.Show(header, body, icon);
+
         m_notifications.Insert(0, new Notification(notification, m_notificationLifetime));
+    }
+
+    private Sprite ResolveQuestRewardIcon(Quest quest)
+    {
+        foreach (QuestReward reward in quest.Rewards)
+        {
+            if (reward.RewardType == QuestRewardType.Sticker)
+            {
+                return GameManager.Sticker.Registry[reward.ID].StickerImage;
+            }
+        }
+        return null;
     }
 
     private enum NotificationType
